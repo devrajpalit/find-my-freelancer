4b87c39bd46907e7455e9ee28f2c826e83112c15
devrajpalit <devraj.palit@gmail.com>
2020-05-31 05:11:14 +0530

    Enhancements II

--- src/home-component.tsx
+++ src/home-component.tsx
@@ -3,7 +3,6 @@ import { IProps, IState, IFreelancer } from './typings';
 import RestService from './shared/rest-service';
 import './home-component.css'
 
-
 class HomeComponent extends React.Component<IProps, IState> {
     restService: RestService;
 
@@ -21,78 +20,81 @@ class HomeComponent extends React.Component<IProps, IState> {
     render() {
         return (this.state && (
             <div className='container'>
-
-                <h1>Find My Freelancer</h1>
-                <div className='row'>
-                    <form className="form-inline">
-                        <div className="form-group mb-2">
-                            {/* <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Category: {this.state.searchFor}
-                            </button>
-                            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                <a className="dropdown-item" onChange={this.handleSearchInput}>Email</a>
-                                <a className="dropdown-item" onChange={this.handleSearchInput}>First Name</a>
-                                <a className="dropdown-item" onChange={this.handleSearchInput}>Last Name</a>
-                            </div> */}
-                            <select className="category dropdown dropdown-toggle" onChange={this.handleSearchTypeInput}>
-                                <option value="email">Email</option>
-                                <option value="first_name">First Name</option>
-                                <option value="last_name">Last Name</option>
-                            </select>
-                        </div>
-                        <div className="form-group mx-sm-3 mb-2">
-                            <input className="form-control" type="text" placeholder="Search" aria-label="Search" onChange={this.handleSearchInput}></input>
-                        </div>
-                    </form>
+                <div className='my-4'>
+                    <h3 className='header-font d-lg-none my-3'>Find My <h1>Freelancer</h1></h3>
+                    {this.renderSearch()}
                 </div>
+
                 <div className='row'>
-                    {/* <div className='find col-xs-6 m-3'>
-                        <h4>Find My</h4>
-                        <h1>Freelancer</h1>
-                    </div> */}
+                    <div className='header-font find d-none d-lg-block col-md-6 text-center'>
+                        <h4 className='title-small'>Find My</h4>
+                        <h1 className='title-big'>Freelancer</h1>
+                    </div>
                     {this.state.dataCurrent.map((freelancer: IFreelancer) => {
                         return (this.renderFreelancerDetails(freelancer));
                     })}
                 </div>
+
                 <div className="footer">
-                    <a className ='ad' href={this.state.ad.url}>
-                        <span className='ad'>
-                            {this.state.ad.company}
-                            {this.state.ad.text}
-                        </span>
+                    <a className='ad' href={this.state.ad.url}>
+                        <div className='ad'>
+                            {this.state.ad.company} : {this.state.ad.text}
+                        </div>
                     </a>
                 </div>
-            </div>)
+            </div>
+        ));
+    }
+
+    private renderSearch() {
+        return (
+            <form className="form-inline flex-grow-1">
+                <div className="input-group">
+                    <div className="input-group-prepend">
+                        <label className="input-group-text">Search By</label>
+                    </div>
+                    <select className="custom-select" onChange={this.handleSearchTypeInput}>
+                        <option value="email">Email</option>
+                        <option value="first_name">First Name</option>
+                        <option value="last_name">Last Name</option>
+                    </select>
+                </div>
+                <div className="input-group flex-grow-1 ml-sm-3">
+                    <input className="form-control flex-grow-1" type="text" placeholder="Search" aria-label="Search" onChange={this.handleSearchInput}></input>
+                </div>
+            </form>
         );
     }
 
     private renderFreelancerDetails(freelancer: IFreelancer) {
         return (
-        <div className='freelancer-info col-xs-12 col-md-4 p-2' key={freelancer.id}>
-            <div className="card" >
-                <img className="card-img-top img-fluid" src={freelancer.avatar} alt="Freelancer's Profile"/>
-                <div className="card-body">
-                    <h5 className="card-title">{freelancer.first_name} {freelancer.last_name}</h5>
-                    <p className="card-text">{freelancer.email}</p>
-                    {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
+            <div className='freelancer-info col-12 col-sm-3' key={freelancer.id}>
+                <div className="card" >
+                    <img className="card-img-top img-fluid" src={freelancer.avatar} alt="Freelancer's Profile" />
+                    <div className="card-body px-1">
+                        <h5 className="card-title mb-1">{freelancer.first_name} {freelancer.last_name}</h5>
+                        <p className="card-text">{freelancer.email}</p>
+                    </div>
                 </div>
             </div>
-        </div>
         );
     }
 
     private getData() {
         this.restService.getData().then(response => {
-            this.setState({
-                page: response.page,
-                per_page: response.per_page,
-                total: response.total,
-                total_pages: response.total_pages,
-                data: response.data,
-                ad: response.ad,
-                dataCurrent: response.data,
-                searchFor: 'email'
-            });
+            if (response) {
+                this.setState({
+                    page: response.page,
+                    per_page: response.per_page,
+                    total: response.total,
+                    total_pages: response.total_pages,
+                    data: response.data,
+                    ad: response.ad,
+                    dataCurrent: response.data,
+                    searchFor: 'email'
+                });
+            }
+
         });
     }
 
@@ -100,7 +102,7 @@ class HomeComponent extends React.Component<IProps, IState> {
         const searchParam = event.target.value.toLowerCase();
         let dataUpdate = this.state.data;
         dataUpdate = dataUpdate.filter((freelancer: IFreelancer) => {
-            switch(this.state.searchFor) {
+            switch (this.state.searchFor) {
                 case 'email':
                     return freelancer.email.toLowerCase().includes(searchParam);
                 case 'first_name':
